f951f8e1a202c54f9a7874957b44df06a0adf9ea
khabib <habib.sharopov2001@mail.ru>
2023-11-09 16:39:04 +0500

    added 0 m3

--- src/modules/information.js
+++ src/modules/information.js
@@ -23,7 +23,7 @@ export const recommendationText = [
       ...levelTextRecommendation[2]
    ].reverse(),
    [
-      ["", "", "", "", "", ""],
+      ["0,0-0,0 m<sup>3</sup>", "0,0-0,0 m<sup>3</sup>", "0,0-0,0 m<sup>3</sup>", "0,0-0,0 m<sup>3</sup>", "0,0-0,0 m<sup>3</sup>", "0,0-0,0 m<sup>3</sup>"],
       ["2,0-2,5 m<sup>3</sup>", "2,5-3,0 m<sup>3</sup>", "3,0-3,5 m<sup>3</sup>", "3,5-4,0 m<sup>3</sup>", "4,0-4,5 m<sup>3</sup>", "4,5-5,0 m<sup>3</sup>"],
       ["2,5-3,0 m<sup>3</sup>", "3,0-3,5 m<sup>3</sup>", "3,5-4,0 m<sup>3</sup>", "4,0-4,5 m<sup>3</sup>", "4,5-5,0 m<sup>3</sup>", "5,0-5,5 m<sup>3</sup>"],
       ["3,0-3,5 m<sup>3</sup>", "3,5-4,0 m<sup>3</sup>", "4,0-4,5 m<sup>3</sup>", "4,5-5,0 m<sup>3</sup>", "5,0-5,5 m<sup>3</sup>", "5,5-6,0 m<sup>3</sup>"],
