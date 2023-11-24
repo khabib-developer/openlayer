053b31186b26e61cedb9dfe0f63f33c08d66a32a
khabib <habib.sharopov2001@mail.ru>
2023-11-24 08:31:52 +0100

    fixed changeFeatureColor

--- src/map/renderColors.js
+++ src/map/renderColors.js
@@ -1,84 +1,102 @@
-import {service} from "../service";
-import {chooseTextColor, delay, removeLoader, showLoader} from "../utils";
-import {Fill, Stroke, Style, Text} from "ol/style";
-import {colorInformation, featureFieldNames, recommendedColorInformation} from "../modules/renderModules";
-
+import { service } from "../service";
+import { chooseTextColor, delay, removeLoader, showLoader } from "../utils";
+import { Fill, Stroke, Style, Text } from "ol/style";
+import {
+  colorInformation,
+  featureFieldNames,
+  recommendedColorInformation,
+} from "../modules/renderModules";
 
 class RenderColors {
-   data = []
-   sections = []
+  data = [];
+  sections = [];
 
-   async setData(sections, value, recommendation = false) {
-      const result = await service(`/api/modul/counter-db/`)
-      if(result) {
-         this.data = result
-         this.sections = sections.getProperties().source.getFeatures()
-         recommendation?this.changeFeaturesColorsWithRecommendedColors(value - 1):this.changeFeaturesColors(value)
-         return this.data
+  async setData(sections, value, recommendation = false) {
+    const result = await service(`/api/modul/counter-db/`);
+    if (result) {
+      this.data = result;
+      this.sections = sections.getProperties().source.getFeatures();
+      recommendation
+        ? this.changeFeaturesColorsWithRecommendedColors(value - 1)
+        : this.changeFeaturesColors(value);
+      return this.data;
+    }
+    return null;
+  }
+
+  async changeData(value, param = "", recommendation) {
+    showLoader();
+    const result = await service(`/api/modul/counter-db/${param}`);
+    if (result && result.length > 0) {
+      this.data = result;
+      recommendation
+        ? this.changeFeaturesColorsWithRecommendedColors(value + 1)
+        : this.changeFeaturesColors(value + 1);
+      removeLoader();
+      return this.data;
+    }
+  }
+
+  changeFeaturesColors(value) {
+    const arr = [];
+
+    this.sections.forEach((feature, i) => {
+      const item = this.data.find(
+        (item) => +item.counter_id === +feature.getProperties()["Kontur_raq"]
+      );
+      if (item === undefined) {
+        arr.push(feature.getProperties()["Kontur_raq"]);
+        return;
       }
-      return null
-   }
-
-   async changeData(value, param = "", recommendation) {
-      showLoader()
-      const result = await service(`/api/modul/counter-db/${param}`)
-      if(result && result.length > 0) {
-         this.data = result
-         recommendation?this.changeFeaturesColorsWithRecommendedColors(value + 1):this.changeFeaturesColors(value + 1)
-         removeLoader()
-         return this.data
+      const level = item[featureFieldNames[value - 1]];
+      if (item) {
+        feature.setStyle(
+          this.style(
+            String(feature.getProperties()["Kontur_raq"]),
+            colorInformation[value - 1][level - 1],
+            chooseTextColor(level)
+          )
+        );
       }
-   }
+    });
+    console.log(JSON.stringify(arr));
+  }
 
-   changeFeaturesColors(value) {
-      this.sections.forEach((feature, i) => {
-         const item = this.data.find(item => +item.counter_id === +feature.getProperties()["Kontur_raq"])
-         const level = item[featureFieldNames[value - 1]]
-         if(item) {
-            feature.setStyle(
-                this.style(
-                    String( feature.getProperties()["Kontur_raq"] ),
-                    colorInformation[value -1][level - 1],
-                    chooseTextColor(level)
-                )
-            )
-         }
-      })
-   }
+  changeFeaturesColorsWithRecommendedColors(index) {
+    this.sections.forEach((feature, i) => {
+      const item = this.data.find(
+        (item) => +item.counter_id === +feature.getProperties()["Kontur_raq"]
+      );
+      const level = item[featureFieldNames[index]];
+      if (item) {
+        feature.setStyle(
+          this.style(
+            String(feature.getProperties()["Kontur_raq"]),
+            [...recommendedColorInformation[index]].reverse()[level - 1],
+            chooseTextColor(level, true)
+          )
+        );
+      }
+    });
+  }
 
-   changeFeaturesColorsWithRecommendedColors(index) {
-      this.sections.forEach((feature, i) => {
-         const item = this.data.find(item => +item.counter_id === +feature.getProperties()["Kontur_raq"])
-         const level = item[featureFieldNames[index]]
-         if(item) {
-            feature.setStyle(
-                this.style(
-                    String( feature.getProperties()["Kontur_raq"] ),
-                    [...recommendedColorInformation[index]].reverse()[level - 1],
-                    chooseTextColor(level, true)
-                )
-            )
-         }
-      })
-   }
-
-   style(text, color, textColor) {
-      return new Style({
-         fill: new Fill({
-            color: color,
-         }),
-         stroke: new Stroke({
-            color: "rgb(0,0,0,0.5)",
-            width: 1,
-         }),
-         text: new Text({
-            text,
-            fill: new Fill({
-               color: textColor,
-            }),
-         }),
-      });
-   }
+  style(text, color, textColor) {
+    return new Style({
+      fill: new Fill({
+        color: color,
+      }),
+      stroke: new Stroke({
+        color: "rgb(0,0,0,0.5)",
+        width: 1,
+      }),
+      text: new Text({
+        text,
+        fill: new Fill({
+          color: textColor,
+        }),
+      }),
+    });
+  }
 }
 
-export default new RenderColors()
+export default new RenderColors();
--- src/map/sections/index.js
+++ src/map/sections/index.js
@@ -1,14 +1,17 @@
 import VectorImageLayer from "ol/layer/VectorImage";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
-import {changeSelectedFeaturesStyle, setDefaultStyle} from "../../hooks/search.hook";
-import {delay} from "../../utils";
+import {
+  changeSelectedFeaturesStyle,
+  setDefaultStyle,
+} from "../../hooks/search.hook";
+import { delay } from "../../utils";
 
 export async function drawSections(map) {
   const source = new VectorSource({
-     url: new URL("../../../data/Kontur.json", import.meta.url),
-     format: new GeoJSON(),
-  })
+    url: new URL("../../../data/Konturlar_new.json", import.meta.url),
+    format: new GeoJSON(),
+  });
 
   const sections = new VectorImageLayer({
     source,
@@ -21,23 +24,21 @@ export async function drawSections(map) {
   map.on("click", (evt) => {
     sections.getFeatures(evt.pixel).then((features) => {
       if (features.length) {
-        setDefaultStyle()
-        changeSelectedFeaturesStyle(features[0], map)
+        setDefaultStyle();
+        changeSelectedFeaturesStyle(features[0], map);
       }
     });
   });
 
-  await checkIfLengthNotEmpty(source)
+  await checkIfLengthNotEmpty(source);
 
-  console.log('loaded')
+  console.log("loaded");
 
   return { sections };
 }
 
 async function checkIfLengthNotEmpty(s) {
-  if (s.getFeatures().length)
-    return s
-  await delay(500)
-  return await checkIfLengthNotEmpty(s)
+  if (s.getFeatures().length) return s;
+  await delay(500);
+  return await checkIfLengthNotEmpty(s);
 }
-
